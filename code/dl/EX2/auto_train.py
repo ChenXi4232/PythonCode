84e7a6e235679af92dccdfddcf5675bf779cc353
ChenXi4232 <2208093719@qq.com>
2024-04-20 12:01:33 +0800

    update

--- code/dl/EX2/auto_train.py
+++ code/dl/EX2/auto_train.py
@@ -19,7 +19,10 @@ def add_comments(file_path, start_line, end_line):
         lines = file.readlines()
 
     for i in range(start_line - 1, end_line):
-        lines[i] = '# ' + lines[i]  # 在行首添加注释符号
+        first_non_space_index = len(lines[i]) - len(lines[i].lstrip())
+        # 在第一个非空格字符后添加注释符号，并保留原有的空格
+        lines[i] = lines[i][:first_non_space_index] + \
+            '# ' + lines[i][first_non_space_index:]
 
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(lines)
--- test.py
+++ test.py
@@ -1,39 +1,12 @@
-from pulp import LpProblem, LpMaximize, LpVariable, lpSum, value
+def add_comments(file_path, start_line, end_line):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-num_products = 3
-num_markets = 3
+    for i in range(start_line - 1, end_line):
+        first_non_space_index = len(lines[i]) - len(lines[i].lstrip())
+        # 在第一个非空格字符后添加注释符号，并保留原有的空格
+        lines[i] = lines[i][:first_non_space_index] + \
+            '# ' + lines[i][first_non_space_index:]
 
-prices = [[10, 9, 8], [9, 8, 7], [8, 7, 6]]
-
-products = [chr(65+i) for i in range(num_products)]
-markets = range(1, num_markets+1)
-shifts = range(1, 5)
-
-# 产品数量变量
-production_vars = LpVariable.dicts(
-    "Production", (products, shifts), lowBound=0, cat='Continuous')
-
-# 目标函数
-avg_prices = []
-for p in products:
-    avg_price = 0
-    for m in markets:
-        avg_price += prices[m-1][products.index(p)]
-    avg_price /= num_markets
-    avg_prices.append(avg_price)
-
-expr = lpSum([avg_prices[i] * production_vars[products[i]][s]
-             for i in range(num_products) for s in shifts])
-
-print(expr)
-
-wages = [10, 9, 8, 7]
-
-shifts_times = [8, 4, 8, 4]
-shifts_wages = [wages[i] * shifts_times[i] for i in range(4)]
-shifts_wages = [shifts_wages[0]] + shifts_wages
-
-total_employees = LpVariable.dicts(
-    "E", range(0, 5), lowBound=0, cat='Continuous')
-total_wages = [lpSum([total_employees[s] * shifts_wages[s] for s in range(5)])]
-print(total_wages)
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
